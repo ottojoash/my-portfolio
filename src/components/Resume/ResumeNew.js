fbb100ea62f27550b13afa8030c4fd100752da19
ottojoash <louisjoshbricks@gmail.com>
2023-03-13 17:01:50 +0300

    pdf

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-//import pdf from "";
+import pdf from "../../Assets/OTTO_JOASH_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "";
+  "https://raw.githubusercontent.com/ottojoash/my-portfolio/master/src/Assets/OTTO_JOASH_CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
